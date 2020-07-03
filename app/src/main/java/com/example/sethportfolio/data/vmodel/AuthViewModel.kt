f1b8eb9bd9ad2b99a16f1a317eb8b468eb50f1a7
Seth Torralba <sethosrictorralba@gmail.com>
2020-07-03 12:34:48 -0500

    Auth: Register Firebase Coroutine Implemented

--- app/src/main/java/com/example/sethportfolio/data/vmodel/AuthViewModel.kt
+++ app/src/main/java/com/example/sethportfolio/data/vmodel/AuthViewModel.kt
@@ -1,35 +1,58 @@
 package com.example.sethportfolio.data.vmodel
 
 import android.view.View
-import androidx.lifecycle.MutableLiveData
-import androidx.lifecycle.ViewModel
+import androidx.lifecycle.*
 import com.example.sethportfolio.data.app.App
 import com.example.sethportfolio.data.app.log
+import com.example.sethportfolio.data.app.onlyNew
 import com.example.sethportfolio.data.model.User
 import com.example.sethportfolio.data.repo.AuthRepo
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers.IO
+import kotlinx.coroutines.Dispatchers.Main
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.withContext
 
 class AuthViewModel:ViewModel() {
-
+    //Repo Instansation
     val repo = AuthRepo()
+    //=====================================
 
+    //Register Items
+    //======================================
     val registerUser by lazy {
         val x = MutableLiveData<User>()
         x.value = User()
         x
     }
 
+    val checkRegister by lazy {
+        MutableLiveData<Boolean>()
+    }
+
+    //Login Items
+    //=======================================
     val loginUser by lazy {
         val x = MutableLiveData<User>()
         x.value = User()
         x
     }
 
-    fun buttonRegisterClicked(view: View) {
+    //Functions
+    //==================================
 
-        //App.instance.log(registerUser.value.toString())
-        //Call Repo register function
-        repo.registerUser(registerUser)
+    fun buttonRegisterClicked(view: View) {
+        //run repo registerUser and run it in corotuine
+        viewModelScope.launch{
+            var x = repo.registerUser(registerUser)
+            withContext(Main) {
+                checkRegister.value = x
+            }
+        }
     }
+
+
+
 
     fun buttonLoginClicked(view:View) {
         App.instance.log(loginUser.value.toString())
