50e0b6ccc74cd1721bbef5f355661991bade4d4b
Seth Torralba <sethosrictorralba@gmail.com>
2020-07-02 20:49:06 -0500

    Setup: Added folder structure and all dependencies

---
+++ app/src/main/java/com/example/sethportfolio/data/app/Utility.kt
@@ -0,0 +1,34 @@
+package com.example.sethportfolio.data.app
+
+import android.content.Context
+import android.util.Log
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+import androidx.appcompat.widget.Toolbar
+import androidx.lifecycle.LifecycleOwner
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.LiveDataReactiveStreams
+import io.reactivex.Flowable
+
+fun Context.log(message:String) {
+    Log.d("STLog", "ST` ${message}" )
+}
+
+fun Context.toast(message:String) {
+    Toast.makeText(this, message, Toast.LENGTH_SHORT).show()
+}
+
+//fun AppCompatActivity.setupToolbar(title:String) {
+//    var myToolbar: Toolbar = this.toolbar
+//    myToolbar.title = title
+//    this.setSupportActionBar(myToolbar)
+//}
+
+fun <T> LiveData<T>.onlyNew(lifecycleOwner: LifecycleOwner): LiveData<T> {
+    if (this.value != null) {
+        var x = Flowable.fromPublisher(LiveDataReactiveStreams.toPublisher(lifecycleOwner, this))
+        x = x.skip(1)
+        return LiveDataReactiveStreams.fromPublisher(x)
+    }
+    return this
+}
